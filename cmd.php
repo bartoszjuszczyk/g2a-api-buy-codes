2d52473d13f626bdb3ceb207727b72cd47259a35
Bartosz Juszczyk <b.juszczyk@bjuszczyk.pl>
2025-07-03 23:58:46 +0200

    Added handling environment variables

--- cmd.php
+++ cmd.php
@@ -8,11 +8,15 @@ declare(strict_types=1);
  * @author Bartosz Juszczyk <b.juszczyk@bjuszczyk.pl>
  * @copyright Copyright (C) 2025
  */
-
 require __DIR__.'/vendor/autoload.php';
 
 $app = new \Symfony\Component\Console\Application('G2A API Code Buy', '1.0.0');
 
 $app->add(new \Juszczyk\Console\BuyCommand());
+
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$dotenv->required(['API_EMAIL', 'API_DOMAIN', 'API_HASH', 'API_CUSTOMER_SECRET']);
+
 
 $app->run();
